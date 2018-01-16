5c2d0eaab41df29c37c68e51a7b9ff93855e04ee
samdd <sam@samdd.me>
2018-01-16 21:27:51 +0000

    :package: Version 1

--- README.md
+++
@@ -1,3 +0,0 @@
-# Crate
-Clean & powerful popup Discord widgets for your website.
-![Demo](https://i.imgur.com/GjHSu16.gif)
--- src/components/Log.tsx
+++
@@ -1,9 +0,0 @@
-export default function (type: 'info' | 'error' | 'warn', message: any, ref?: any) {
-  let color = ''
-  if (type === 'warn') {
-    color = '#fff816'
-  } else if (type === 'error') {
-    color = '#f9aeae'
-  }
-  console[type](`%c☄️ [Crate] %c${message}\n`, 'color: #40b8ff', `color: ${color}`, ref)
-}
