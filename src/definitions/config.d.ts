1dd9517c64d0ddc75cf0d1b7b89ebcf01a92cea3
samdd <sam@samdd.me>
2018-02-12 19:04:38 +0000

    :recycle:

--- src/definitions/config.d.ts
+++ src/definitions/config.d.ts
@@ -4,34 +4,35 @@ export interface Config {
   /**
    * WidgetBot widget options
    */
-  server   : string               // Your guilds ID
-  channel  : string               // Channel ID
-  options ?: string               // WidgetBot widget options
-  beta    ?: boolean              // Whether to use the beta domain or not
-  buttons ?: {                    // Change text of buttons
-    primary   ?: string
-    secondary ?: string
+  server: string                // Your guilds ID
+  channel: string               // Channel ID
+  options?: string              // WidgetBot widget options
+  beta?: boolean                // Whether to use the beta domain or not
+  buttons?: {                   // Change text of buttons
+    primary?: string
+    secondar?: string
   }
-  username: string                 // Use a specific username for the guest chatter
+  username: string              // Use a specific username for the guest chatter
 
   /**
    * Aesthetic options
    */
-  logo    ?: Url | 'intercom' | 'discord' | any | {
-    url   : Url
-    size ?: string
+  logo?: Url | 'intercom' | 'discord' | any | {
+    url: Url
+    size?: string
   }
-  style   ?: 'material' | 'default'
-  colors  ?: {
-    toggle     ?: string          // Crate toggle button color
-    background ?: string          // WidgetBot widget background
-    button     ?: string          // WidgetBot `Start chatting` button color
+  scheme?: 'dark' | 'light'     // Whether to show dark or light toasts
+  style?: 'material' | 'default'// Toggle button style
+  colors?: {
+    toggle?: string             // Crate toggle button color
+    background?: string         // WidgetBot widget background
+    button?: string             // WidgetBot `Start chatting` button color
   }
   position: {
     x: 'left' | 'right'
     y: 'top' | 'bottom'
   }
-  contained: boolean              // Display modals in iframe or in the window
+  contained: boolean            // Display modals in iframe or in the window
 
   /**
    * Notifications
@@ -41,24 +42,23 @@ export interface Config {
       enable: boolean
     }
     toasts?: {
-      enable          : boolean  // Whether to enable toasts or not
-      visibilityTime ?: number   // Max amount of time the toasts should be visible for (set to 0 to disable timeout)
-      maxMessages    ?: number   // Max amount of messages to display on screen
-      maxHeight      ?: string   // Max height of the toast container, CSS `calc()` can be used
+      enable: boolean           // Whether to enable toasts or not
+      visibilityTime?: number   // Max amount of time the toasts should be visible for (set to 0 to disable timeout)
+      maxMessages?: number      // Max amount of messages to display on screen
+      maxHeight?: string        // Max height of the toast container, CSS `calc()` can be used
     }
   }
 
   /**
    * General options
    */
-  delay  ?: boolean           // Only load the widget once the button has been clicked
-  debug  ?: boolean           // Debug crate
+  delay?: boolean               // Only load the widget once the button has been clicked
+  debug?: boolean               // Debug crate
 
   /**
    * Overrides
    */
-  scheme  ?: 'dark' | 'light' // Override to prevent using the same scheme as the widget
-  domain  ?: string           // Override the domain
-  url     ?: string           // Override the Widget URL
-  query   ?: any              // Override the query string
+  domain?: string               // Override the domain
+  url?: string                  // Override the Widget URL
+  query?: any                   // Override the query string
 }
