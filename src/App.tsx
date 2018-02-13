48335973fee199a7b6fef05b24ff3f3f2ac4a7f7
samdd <sam@samdd.me>
2018-02-13 04:33:42 +0000

    Raven

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+/**
+ * RavenJS logging
+ */
+const Raven = require('raven-js')
+Raven.config('https://60c8644853c540649cfdf0fe4a30bfe4@sentry.io/287303', {
+  whitelistUrls: [/widgetbot\.io/, /crate\.widgetbot\.io/, /localhost:3100/],
+  includePaths: [/https?:\/\/crate.widgetbot\.io/]
+}).install()
+
 declare var window: any
 import { Config } from './definitions/config'
 import * as ReactDOM from "react-dom"
@@ -10,295 +19,296 @@ import log from './components/Log'
 import { Icons } from './components/Icons'
 import jss from './jss/App'
 
-
-// Crate sandbox
-let global = window.globalCrate = {
-  insertionPoint: /* :smirk: */ document.createElement('div'),
-  sessions: 0,
-  ReactGA: require('react-ga')
-}
-global.insertionPoint.setAttribute('documentation', 'https://docs.widgetbot.io')
-global.insertionPoint.classList.add('crate')
-// Wait for the DOM to load before inserting
-if (document.body) {
-  document.body.appendChild(global.insertionPoint)
-} else {
-  document.addEventListener('DOMContentLoaded', () => {
+Raven.context(() => {
+  // Crate sandbox
+  let global = window.globalCrate = {
+    insertionPoint: /* :smirk: */ document.createElement('div'),
+    sessions: 0,
+    ReactGA: require('react-ga')
+  }
+  global.insertionPoint.setAttribute('documentation', 'https://docs.widgetbot.io')
+  global.insertionPoint.classList.add('crate')
+  // Wait for the DOM to load before inserting
+  if (document.body) {
     document.body.appendChild(global.insertionPoint)
-  })
-}
-
-// Crate console message
-console.log(`%c+%chttps://crate.widgetbot.io\n%c\u2604\uFE0F Popup Discord chat widgets for your website.`, `font-size: 1px; margin-left: 40px; padding: 20px 20px; line-height: 50px;background: url("https://i.imgur.com/S7IIIbE.png"); background-repeat: no-repeat; background-size: 40px 40px; color: transparent;`, `padding-left: 2px; font-size: 14px; color: #7289DA; font-family: "Roboto", sans-serif`, `padding-left: 15px; font-size: 12px; font-family: "Roboto", sans-serif;`);
-
-/**
- * Due to React trying enforcing a "strict" state handler,
- * all states for Crate are manually handled in a class that
- * doesn't extend React.Component; This results in a speed
- * increase and the ability to recreate the live-state
- * to scripts outside the React Renderer class.
- */
-class StateHandler {
-  state = {
-    view: {
-      opened: false,
-      open: false,
-      loading: true,
-      modalOpen: false
-    },
-    modal: {
-
-    },
-    /**
-     * Default configuration
-     */
-    config: {
-      server: '299881420891881473',
-      channel: '355719584830980096',
-      options: '0002',
-      beta: false,
-      debug: false,
-
-      logo: {
-        url: Icons()
-      },
-      theme: 'default',
-      colors: {
-        toggle: '#7289DA'
-      },
-
-      notifications: {
-        indicator: {
-          enable: true
-        },
-        toasts: {
-          enable: true,
-          visibilityTime: 10,
-          maxMessages: 5,
-          maxHeight: 'calc(70% - 100px)'
-        }
-      },
-
-      mobile: {
-        maxWidth: 500,
-        maxHeight: 500
-      },
-
-      position: {
-        x: 'right',
-        y: 'bottom'
-      },
-
-      delay: false
-    },
-    notifications: {
-      unread: 0,
-      pinged: false,
-      messages: []
-    },
-    classes: {},
-    session: (Math.floor(Math.pow(10, 15) + Math.random() * 9 * Math.pow(10, 15)) + +new Date()).toString()
-  }
-  react: any
-  node: any
-
-  constructor(config) {
-    if (!window.crate) window.crate = this
-    ParseConfig(this.state, config).then((config) => {
-      this.setState({
-        classes: jss(config),
-        config: config
-      })
-
-      if (!config.delay) {
-        setTimeout(() => {
-          this.setState({
-            view: {
-              ...this.state.view,
-              opened: true
-            }
-          })
-        }, 3000)
-      }
-
-      // Mount DOM node
-      this.node = document.createElement('div')
-      this.node.classList.add(`crate-${global.sessions}`)
-      global.insertionPoint.appendChild(this.node)
-      global.sessions++
-      ReactDOM.render(
-        // @ts-ignore custom state handler
-        <Renderer api={this} ref={renderer => { this.react = renderer }} />, this.node
-      )
-
-      // Analytics
-      let { ReactGA } = global
-      ReactGA.initialize('UA-107130316-3', { debug: config.debug })
-      ReactGA.pageview(window.location.origin)
-      ReactGA.set({
-        server: config.server,
-        channel: config.channel
-      })
-    }).catch((error) => {
-      log('error', `Invalid configuration!\n${error}\n\nrefer to https://docs.widgetbot.io`)
+  } else {
+    document.addEventListener('DOMContentLoaded', () => {
+      document.body.appendChild(global.insertionPoint)
     })
   }
 
-  // Custom state handler
-  setState(nextState: any) {
-    Object.keys(nextState).forEach(state => {
-      // Force JSS re-render
-      if (nextState && nextState.config && JSON.stringify(nextState.config) !== JSON.stringify(this.state.config)) {
-        this.config(nextState.config)
-      }
-      this.state[state] = nextState[state]
-    })
-    // Force re-render of the renderer
-    if (this.react) this.react.forceUpdate()
-  }
+  // Crate console message
+  console.log(`%c+%chttps://crate.widgetbot.io\n%c\u2604\uFE0F Popup Discord chat widgets for your website.`, `font-size: 1px; margin-left: 40px; padding: 20px 20px; line-height: 50px;background: url("https://i.imgur.com/S7IIIbE.png"); background-repeat: no-repeat; background-size: 40px 40px; color: transparent;`, `padding-left: 2px; font-size: 14px; color: #7289DA; font-family: "Roboto", sans-serif`, `padding-left: 15px; font-size: 12px; font-family: "Roboto", sans-serif;`);
 
-  // Deep merges the new config with the current config
-  config(config: any) {
-    ParseConfig(this.state, config, true).then((config) => {
-      this.setState({
-        classes: jss(config),
-        config: config
-      })
-    }).catch((error) => {
-      log('error', `Invalid configuration!\n${error}\n\nrefer to https://docs.widgetbot.io`)
-    })
-  }
-}
-
-/**
- * Group APIs under this class
- */
-class Crate extends StateHandler {
-  toggle(open?: boolean) {
-    open = typeof open === 'boolean' ? open : !this.state.view.open
-    this.setState({
+  /**
+   * Due to React trying enforcing a "strict" state handler,
+   * all states for Crate are manually handled in a class that
+   * doesn't extend React.Component; This results in a speed
+   * increase and the ability to recreate the live-state
+   * to scripts outside the React Renderer class.
+   */
+  class StateHandler {
+    state = {
       view: {
-        ...this.state.view,
-        open: open,
-        opened: true
+        opened: false,
+        open: false,
+        loading: true,
+        modalOpen: false
+      },
+      modal: {
+
+      },
+      /**
+       * Default configuration
+       */
+      config: {
+        server: '299881420891881473',
+        channel: '355719584830980096',
+        options: '0002',
+        beta: false,
+        debug: false,
+
+        logo: {
+          url: Icons()
+        },
+        theme: 'default',
+        colors: {
+          toggle: '#7289DA'
+        },
+
+        notifications: {
+          indicator: {
+            enable: true
+          },
+          toasts: {
+            enable: true,
+            visibilityTime: 10,
+            maxMessages: 5,
+            maxHeight: 'calc(70% - 100px)'
+          }
+        },
+
+        mobile: {
+          maxWidth: 500,
+          maxHeight: 500
+        },
+
+        position: {
+          x: 'right',
+          y: 'bottom'
+        },
+
+        delay: false
       },
       notifications: {
         unread: 0,
         pinged: false,
         messages: []
-      }
-    })
+      },
+      classes: {},
+      session: (Math.floor(Math.pow(10, 15) + Math.random() * 9 * Math.pow(10, 15)) + +new Date()).toString()
+    }
+    react: any
+    node: any
 
-    /**
-     * Stop the body from scrolling
-     */
-    if (window.innerWidth <= this.state.config.mobile.maxWidth || window.innerHeight <= this.state.config.mobile.maxHeight) {
-      if (open) {
-        document.body.style.overflow = 'hidden'
-      } else {
-        document.body.style.overflow = ''
-      }
+    constructor(config) {
+      if (!window.crate) window.crate = this
+      ParseConfig(this.state, config).then((config) => {
+        this.setState({
+          classes: jss(config),
+          config: config
+        })
+
+        if (!config.delay) {
+          setTimeout(() => {
+            this.setState({
+              view: {
+                ...this.state.view,
+                opened: true
+              }
+            })
+          }, 3000)
+        }
+
+        // Mount DOM node
+        this.node = document.createElement('div')
+        this.node.classList.add(`crate-${global.sessions}`)
+        global.insertionPoint.appendChild(this.node)
+        global.sessions++
+        ReactDOM.render(
+          // @ts-ignore custom state handler
+          <Renderer api={this} ref={renderer => { this.react = renderer }} />, this.node
+        )
+
+        // Analytics
+        let { ReactGA } = global
+        ReactGA.initialize('UA-107130316-3', { debug: config.debug })
+        ReactGA.pageview(window.location.origin)
+        ReactGA.set({
+          server: config.server,
+          channel: config.channel
+        })
+      }).catch((error) => {
+        log('error', `Invalid configuration!\n${error}\n\nrefer to https://docs.widgetbot.io`)
+      })
     }
 
-    /**
-     * Add the meta tag, for chrome header color
-     */
-    (() => {
-      let meta: HTMLMetaElement = document.querySelector('meta[name=theme-color]')
-      if (meta) {
-        if (meta.getAttribute('default') === null) {
-          meta.setAttribute('default', meta.content || '')
+    // Custom state handler
+    setState(nextState: any) {
+      Object.keys(nextState).forEach(state => {
+        // Force JSS re-render
+        if (nextState && nextState.config && JSON.stringify(nextState.config) !== JSON.stringify(this.state.config)) {
+          this.config(nextState.config)
         }
-      } else {
-        meta = document.createElement('meta')
-        meta.setAttribute('name', 'theme-color')
-        meta.setAttribute('default', '')
-        document.head.appendChild(meta)
-      }
+        this.state[state] = nextState[state]
+      })
+      // Force re-render of the renderer
+      if (this.react) this.react.forceUpdate()
+    }
+
+    // Deep merges the new config with the current config
+    config(config: any) {
+      ParseConfig(this.state, config, true).then((config) => {
+        this.setState({
+          classes: jss(config),
+          config: config
+        })
+      }).catch((error) => {
+        log('error', `Invalid configuration!\n${error}\n\nrefer to https://docs.widgetbot.io`)
+      })
+    }
+  }
+
+  /**
+   * Group APIs under this class
+   */
+  class Crate extends StateHandler {
+    toggle(open?: boolean) {
+      open = typeof open === 'boolean' ? open : !this.state.view.open
+      this.setState({
+        view: {
+          ...this.state.view,
+          open: open,
+          opened: true
+        },
+        notifications: {
+          unread: 0,
+          pinged: false,
+          messages: []
+        }
+      })
+
       /**
-       * Timeout so the transition fades with the chrome header
+       * Stop the body from scrolling
        */
-      setTimeout(() => {
+      if (window.innerWidth <= this.state.config.mobile.maxWidth || window.innerHeight <= this.state.config.mobile.maxHeight) {
         if (open) {
-          let color = this.state.config.colors['background'] || this.state.config['scheme'] === 'light' ? '#ffffff' : '#36393E'
-          meta.setAttribute('content', color)
+          document.body.style.overflow = 'hidden'
         } else {
-          meta.setAttribute('content', meta.getAttribute('default'))
+          document.body.style.overflow = ''
         }
-      }, 100)
-    })()
-
-    let { ReactGA } = global
-    ReactGA.event({
-      category: 'Toggle',
-      action: this.state.view.open ? 'Open' : 'Close'
-    })
-  }
-
-  modal(open: boolean = !this.state.view.modalOpen) {
-    this.setState({
-      view: {
-        ...this.state.view,
-        modalOpen: open
       }
-    })
-    let { ReactGA } = global
-    ReactGA.event({
-      category: 'Modal',
-      action: this.state.view.modalOpen ? 'Open' : 'Close'
-    })
-  }
 
-  user(user) {
-    this.setState({
-      view: {
-        ...this.state.view,
-        modalOpen: true
-      },
-      modal: {
-        type: 'user',
-        data: user
-      }
-    })
+      /**
+       * Add the meta tag, for chrome header color
+       */
+      (() => {
+        let meta: HTMLMetaElement = document.querySelector('meta[name=theme-color]')
+        if (meta) {
+          if (meta.getAttribute('default') === null) {
+            meta.setAttribute('default', meta.content || '')
+          }
+        } else {
+          meta = document.createElement('meta')
+          meta.setAttribute('name', 'theme-color')
+          meta.setAttribute('default', '')
+          document.head.appendChild(meta)
+        }
+        /**
+         * Timeout so the transition fades with the chrome header
+         */
+        setTimeout(() => {
+          if (open) {
+            let color = this.state.config.colors['background'] || this.state.config['scheme'] === 'light' ? '#ffffff' : '#36393E'
+            meta.setAttribute('content', color)
+          } else {
+            meta.setAttribute('content', meta.getAttribute('default'))
+          }
+        }, 100)
+      })()
 
-    let { ReactGA } = global
-    ReactGA.event({
-      category: 'UserPopup',
-      action: 'Open'
-    })
-  }
+      let { ReactGA } = global
+      ReactGA.event({
+        category: 'Toggle',
+        action: this.state.view.open ? 'Open' : 'Close'
+      })
+    }
 
-  show() {
-    if (!global.insertionPoint.contains(this.node))
-      global.insertionPoint.appendChild(this.node)
-  }
+    modal(open: boolean = !this.state.view.modalOpen) {
+      this.setState({
+        view: {
+          ...this.state.view,
+          modalOpen: open
+        }
+      })
+      let { ReactGA } = global
+      ReactGA.event({
+        category: 'Modal',
+        action: this.state.view.modalOpen ? 'Open' : 'Close'
+      })
+    }
 
-  hide() {
-    if (global.insertionPoint.contains(this.node))
+    user(user) {
+      this.setState({
+        view: {
+          ...this.state.view,
+          modalOpen: true
+        },
+        modal: {
+          type: 'user',
+          data: user
+        }
+      })
+
+      let { ReactGA } = global
+      ReactGA.event({
+        category: 'UserPopup',
+        action: 'Open'
+      })
+    }
+
+    show() {
+      if (!global.insertionPoint.contains(this.node))
+        global.insertionPoint.appendChild(this.node)
+    }
+
+    hide() {
+      if (global.insertionPoint.contains(this.node))
+        global.insertionPoint.removeChild(this.node)
+    }
+
+    remove() {
+      ReactDOM.unmountComponentAtNode(this.node)
       global.insertionPoint.removeChild(this.node)
+    }
   }
 
-  remove() {
-    ReactDOM.unmountComponentAtNode(this.node)
-    global.insertionPoint.removeChild(this.node)
-  }
-}
+  window.Crate = Crate;
 
-window.Crate = Crate;
+  // Load crate from inside script tag
+  (() => {
+    let config = document.currentScript && document.currentScript.innerHTML
+    if (config) {
+      eval(config)
+    }
+  })();
 
-// Load crate from inside script tag
-(() => {
-  let config = document.currentScript && document.currentScript.innerHTML
-  if (config) {
-    eval(config)
-  }
-})();
-
-// Crate Event
-(() => {
-  var event = document.createEvent('Event')
-  // @ts-ignore
-  event.Crate = Crate
-  event.initEvent('crate', true, false)
-  window.dispatchEvent(event)
-})()
+  // Crate Event
+  (() => {
+    var event = document.createEvent('Event')
+    // @ts-ignore
+    event.Crate = Crate
+    event.initEvent('crate', true, false)
+    window.dispatchEvent(event)
+  })()
+})
