aae3ee32183bf29c9eff0a067f26b6839e7fff0d
Sam Denty <samddenty@gmail.com>
2018-06-27 08:28:22 +0100

    async iife

--- src/api/util.ts
+++ src/api/util.ts
@@ -1,13 +1,6 @@
 import Options from '../types/options'
 import observe from '../util/observe'
 
-console.log(
-  `%c+%chttps://widgetbot.io\n%cPopup Discord chat widgets for your website.`,
-  `font-size: 1px; margin-bottom: 5px; margin-left: 40px; padding: 10px 15px; line-height: 12px;background: url("https://i.imgur.com/S7IIIbE.png"); background-repeat: no-repeat; background-size: 30px; color: transparent;`,
-  `padding-left: 2px; font-size: 14px; color: #7289DA; font-family: "Roboto", sans-serif`,
-  `padding-left: 15px; font-size: 11px; font-family: "Roboto", sans-serif; `
-)
-
 export const enhancer =
   (process.env.NODE_ENV === 'development' &&
     (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
--- src/umd.ts
+++ src/umd.ts
@@ -1,12 +1,34 @@
 import Crate from './api'
 
+console.log(
+  `%c+%chttps://widgetbot.io\n%cPopup Discord chat widgets for your website.`,
+  `font-size: 1px; margin-bottom: 5px; margin-left: 40px; padding: 10px 15px; line-height: 12px;background: url("https://i.imgur.com/S7IIIbE.png"); background-repeat: no-repeat; background-size: 30px; color: transparent;`,
+  `padding-left: 2px; font-size: 14px; color: #7289DA; font-family: "Roboto", sans-serif`,
+  `padding-left: 15px; font-size: 11px; font-family: "Roboto", sans-serif; `
+)
+
 if (window) {
   ;(window as any).Crate = Crate
 
   // Evaluate content inside <script> tag
-  if (document && document.currentScript) {
-    const script = document.currentScript
-    eval(script.innerHTML)
+  if (document) {
+    const { currentScript: script } = document
+    if (script && !script.getAttribute('no-eval')) {
+      const asyncAwait = (() => {
+        try {
+          eval('eval("(async function() {})")')
+        } catch (e) {
+          return false
+        }
+        return true
+      })()
+
+      eval(
+        asyncAwait
+          ? `(async function() {${script.innerHTML}})()`
+          : script.innerHTML
+      )
+    }
   }
 }
 
