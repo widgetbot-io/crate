e155ef54343ab94bc1a51a730f8c0a05060c29a0
Daave <daave@widgetbot.io>
2020-04-12 02:06:37 +0100

    Update shard

--- demo/src/index.ts
+++ demo/src/index.ts
@@ -1,21 +1,9 @@
 import Crate from '../../src'
 
 const crate = new Crate({
-  // server: '436136130035843082',
-  // channel: '436136130035843084',
-  // shard: 'http://localhost:3000',
-  shard: 'https://cl1.widgetbot.io',
+  shard: 'https://e.widgetbot.io',
   server: '299881420891881473',
   channel: '355719584830980096',
-  // glyph: ['https://samdd.me/favicon.ico', '50%'],
-  css: `
-  &:not(.open) .button {
-    background: blue;
-    &:hover {
-      background: red;
-    }
-  }
-`
   // defer: true
 })
 
